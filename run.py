ac3081bac94471200da164eea03778b18b05c270
gjwilson20 <166746021+gjwilson20@users.noreply.github.com>
2024-11-12 14:12:11 +0000

    created blank grid and set the grid size

--- run.py
+++ run.py
@@ -1,3 +1,12 @@
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+import random
+
+#Initialize the grid size
+GRID_SIZE = 5
+
+#Create a blank grid
+def create_grid(size):
+    return[["." for _ in range(size)] for _ in range(size)]
+
