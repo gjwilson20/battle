8fdeb2321cd90ef7693fead3374ac52e6dca593e
gjwilson20 <166746021+gjwilson20@users.noreply.github.com>
2024-11-12 14:55:07 +0000

    added main game function and created game grid whilst displaying to player

--- run.py
+++ run.py
@@ -8,5 +8,21 @@ GRID_SIZE = 5
 
 #Create a blank grid
 def create_grid(size):
-    return[["." for _ in range(size)] for _ in range(size)]
+    return[["O" for _ in range(size)] for _ in range(size)]
+
+# Display the grid to the player
+def print_grid(grid):
+    for row in grid:
+        print(" ".join(row))
+    print()
+
+
+
+
+#Main game function
+def play_battleships():
+
+    #Create game grid
+    grid = create_grid (GRID_SIZE)
+    attempts = 0
 
